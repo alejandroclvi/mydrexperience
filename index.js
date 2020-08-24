243be791e993bd2d95b84228e64b8f68a4ccb1e3
Manuel Calvino <mcalvinolaguardia@gmail.com>
2020-08-24 11:30:27 -0400

    configured apollo client

--- index.js
+++ index.js
@@ -3,7 +3,24 @@
  */
 import 'react-native-gesture-handler'; // polyfill for react-native-navigation
 import {AppRegistry} from 'react-native';
+import React from 'react';
 import App from './src/App';
 import {name as appName} from './app.json';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
 
-AppRegistry.registerComponent(appName, () => App);
+
+const Client = () => {
+  const client = new ApolloClient({
+    link: createHttpLink({ uri: 'http://localhost:5000/graphql' }),
+    cache: new InMemoryCache()
+  });
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>)
+}
+
+AppRegistry.registerComponent(appName, () => Client);
---
+++ ios/Pods/Target Support Files/Pods-mydrexperience/Pods-mydrexperience-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mydrexperience : NSObject
+@end
+@implementation PodsDummy_Pods_mydrexperience
+@end
